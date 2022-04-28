37abd823da8488bf3523fc1df3574c2106162146
amonull <90932550+amonull@users.noreply.github.com>
2022-04-28 17:18:15 +0100

    Add files via upload

---
+++ charachters.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python3
+# check main.py to see the problems with using this path
+
+import pygame
+import random
+from images import image_handler
+from sounds import sound_handler
+
+class Charachter:
+    """this class makes charachters adds hitboxes and draws them on the screen"""
+    def __init__(self, WIDTH, HEIGHT, X_axis, Y_axis, IMAGE, window):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+        self.X_axis = X_axis
+        self.Y_axis = Y_axis
+        self.IMAGE = IMAGE
+        self.window = window
+    def hitbox(self): return pygame.Rect(self.X_axis, self.Y_axis, self.WIDTH, self.HEIGHT)
+    def draw_charachter(self, hitbox): return self.window.blit(image_handler.output(self.IMAGE), (hitbox.x, hitbox.y))
+
+class barrier(Charachter):
+    """class to handle with the bases/barriers"""
+    def __init__(self, WIDTH, HEIGHT, X_axis, Y_axis, IMAGE, window):
+        # inheriting __init__ from charachter
+        super().__init__(WIDTH, HEIGHT, X_axis, Y_axis, IMAGE, window)
+
+class enemy(Charachter):
+    """class to deal with enemies"""
+    def __init__(self, enemy_list, WIDTH, HEIGHT, X_axis, Y_axis, IMAGE, window, speed, direction) -> None: #DETERMINE ENEMY TYPE IN MAIN
+        # inheriting __init__ from charachter
+        super().__init__(WIDTH, HEIGHT, X_axis, Y_axis, IMAGE, window)
+        self.enemy_list = enemy_list
+        self.speed = speed
+        self.direction = direction
+        # IMPORTANT: direction var must be kept here if handled by {def enemy_move()} it will constantly loop over it resetting its value to whatever was set originally
+
+    def generator(self, amount, spacing):
+        """spawns in charachters, inherits from Charachter class to make hitboxes and draw them on screen also adds them to a list"""
+        for enemy in range(amount):
+            # creating object by using inherited class and creates hitboxes for the objects
+            enemy = Charachter(self.WIDTH, self.HEIGHT, self.X_axis, self.Y_axis, self.IMAGE, self.window).hitbox()
+            self.enemy_list.append(enemy)
+            self.X_axis += spacing
+    def draw_generator(self, charachter):
+        """places enemies in the list on to the screen"""
+        return [charachter.draw_charachter(enemy) for enemy in self.enemy_list]
+
+    def enemy_move(self, window_width):
+        """deals with enemy movement from left to right and below"""
+        for enemy in self.enemy_list:
+            max_enemy_x = max(sorted(self.enemy_list)).x + self.WIDTH #moves enemy when it touches side of screen (max has WIDHT beacuse enemy x starts from its top left corner so min doesnt need it)
+            min_enemy_x = min(sorted(self.enemy_list)).x
+
+            # this if statement kickstarts enemy movement and handles their direction
+            if self.direction == "right":
+                enemy.x += self.speed
+            elif self.direction == "left":
+                enemy.x -= self.speed
+
+            if max_enemy_x == window_width: #checks if highest enemy x num reacher to end of screen
+                enemy.y += 10
+                self.direction = "left"
+            elif min_enemy_x == 0: #checks if lowest enemy x reached end of screen
+                enemy.y += 10
+                self.direction = "right"
+
+    def probability(self):
+        """probability counter to determine if ship laser can destroy enemy laser"""
+        if self.enemy_laser['normal_laser']: #always returns true
+            return True
+        elif self.enemy_laser['fast_laser']: #1/2 chance to return true 
+            chance = random.randint(0,2)
+            if chance == 0:
+                return True
+        elif self.enemy_laser['wiglly_laser']: #1/3 chance to return true
+            chance = random.randint(0,3)
+            if chance == 0:
+                return True
+        else: #if laser is none of the above returns an error and stops the code
+            raise Exception("wrong laser type entered")
+    def enemy_laser(self):
+        """handles enemy laser shooting"""
+        pass
+
+class projectile(enemy):
+    """deals with projectiles"""
+    lasers_list = []
+
+    def __init__(self, ship, window, score, *lists, **enemy_lasers) -> None:
+        self.window = window
+        self.ship = ship
+        self.score = score
+        self.enemy_lasers = enemy_lasers
+        self.lists = lists
+
+    def handle_lasers(self, LASER_SPEED):
+        """adds laser to screen, deals with its movement, and removes from list when an event occurs"""
+        # the [:] before calling lists allows for chaning the lists more accurately
+        for laser in self.lasers_list[:]:
+            for laser in self.lasers_list: self.window.blit(image_handler.LASER, (laser)) #draws laser
+            laser.y -= LASER_SPEED #moves laser
+            if laser.y < 0: #destroy laser if it goes over the screen
+                self.lasers_list.remove(laser)
+            
+            # if laser hits enemy or barrier destroy laser, kill enemy and ad appropriate score to player score
+            for enemy in self.lists[0][:]:
+                if laser.colliderect(enemy):
+                    self.lists[0].remove(enemy)
+                    self.score += 10
+                    self.lasers_list.remove(laser)
+                    explosion = Charachter(enemy.width, enemy.height, enemy.x, enemy.y, image_handler.EXPLOSION_PURPLE, self.window).draw_charachter(enemy)
+            for enemy in self.lists[1][:]:
+                if laser.colliderect(enemy):
+                    self.lists[1].remove(enemy)
+                    self.score += 10
+                    self.lasers_list.remove(laser)
+                    explosion = Charachter(enemy.width, enemy.height, enemy.x, enemy.y, image_handler.EXPLOSION_GREEN, self.window).draw_charachter(enemy)
+            for enemy in self.lists[2][:]:
+                if laser.colliderect(enemy):
+                    self.lists[2].remove(enemy)
+                    self.score += 10
+                    self.lasers_list.remove(laser)
+                    explosion = Charachter(enemy.width, enemy.height, enemy.x, enemy.y, image_handler.EXPLOSION_BLUE, self.window).draw_charachter(enemy)
+
+    def shoot_lasers(self): #dependent on button press cant work at the same place with handle lasers
+        """creates laser hitbox and adds it to a list"""
+        if not self.lasers_list: #checks if the list is empty
+            laser = pygame.Rect(self.ship.x + self.ship.width//2, self.ship.y + self.ship.height//2 - 7, 5, 15) # last numbedlers are width and height (theres a -7 to make bullet placement more accurate and so it doesnt show)
+            self.lasers_list.append(laser)
---
+++ main.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+# this environemt path changes depending on where python path is on venv or on system
+
+# THE x AND y AXIS ARE HARD CODED
+# ^-- to change the space inbetween enemy to enemy or barrier to barrier the new x axis must be calculated
+#to add more barrier more barrier_health variables must be added
+
+import pygame
+from images import image_handler
+from sounds import sound_handler
+from scores import save_score
+import charachters
+
+pygame.init() # safely initilizing all pygame modules
+
+# [GENERAL] consts/vars
+score = 0
+hi_score = save_score.read_scores()
+
+# [WINDOW]
+width, height = 800, 600
+WINDOW = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Space Invaders Game Project")
+
+# [COLOURS]
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+RED = pygame.Color(255, 0, 0)
+GREEN = pygame.Color(0, 255, 0)
+BLUE = pygame.Color(0, 0, 255)
+
+# defining how many frames per second the game updates at
+FPS = 60
+
+# objects/sprites and object attributes
+# images/sounds/fonts gotten from https://github.com/leerob/space-invaderss
+
+# generating empty list for enemies
+enemy1, enemy2, enemy3 = [], [], []
+# keep barrirs in lists an attempt to change that to dict would result in unmutable barriers
+barriers = []
+
+# constants for overall functionality
+ship_speed = 3
+enemy_speed = 1
+laser_speed = 5
+DEFAULT_WIDTH, DEFAULT_HEIGHT = 50, 50
+SHIP_X, SHIP_Y = 375, 520
+BARRIER_X, BARRIER_Y = 175, 455
+ENEMY1_X, ENEMY1_Y = 35, 375
+ENEMY2_X, ENEMY2_Y = 35, 300
+ENEMY3_X, ENEMY3_Y = 35, 225
+direction = "right"
+
+
+def change_window(**hit_box):
+    """adds sprites to screen"""
+    WINDOW.blit(image_handler.BG_IMG,(0,0)) #always keep on top so images added later on are not behind background image
+    hit_box['SHIP'].draw_charachter(hit_box['SHIP_HITBOX']) #adds ship to screen
+    hit_box['LASERS'].handle_lasers(laser_speed) #creates and handles lasers movement
+    hit_box['ENEMY1'].draw_generator(hit_box['ENEMY1']) #***IMPORTANT*** enemy dying animation too quick try to add here and destroy when a func is run
+    hit_box['ENEMY2'].draw_generator(hit_box['ENEMY2'])
+    hit_box['ENEMY3'].draw_generator(hit_box['ENEMY3'])
+    pygame.display.update()
+
+def endgame_screen():
+    """sceen to come up when the player wins or looses"""
+    pass
+
+def main(**sprites):
+    """main function that runs the program"""
+    SHIP_HITBOX = sprites['SHIP'].hitbox()
+    LASERS = charachters.projectile(SHIP_HITBOX, WINDOW, score, enemy1, enemy2, enemy3, barriers, normal_laser=0, fast_laser=0, wiglly_laser=0) #might not work because of the way hitbox is made (doesnt return anything dontknow whether it can access x, y and width, height)
+
+    clock = pygame.time.Clock()
+    run = True
+
+    # pygame runs on a forever loop untill the loop is false
+    while run:
+        clock.tick(FPS) # controls the speed of while loop
+        for event in pygame.event.get():
+
+            # [QUIT]
+            run = False if event.type == pygame.QUIT else True # sets run to False which kills the while loop ending the game
+
+            # [SHOOT]
+            if event.type == pygame.KEYDOWN: #shoots ships bullets
+                if event.key == pygame.K_SPACE:
+                    LASERS.shoot_lasers()
+                elif event.key == pygame.K_RCTRL: #added in so you can move and shoot with one hand
+                    LASERS.shoot_lasers()
+
+        # [MOVEMENT]
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and SHIP_HITBOX.x - ship_speed > 0: # [LEFT] checks to make sure charachter hitbox wont go off screen
+            SHIP_HITBOX.x -= ship_speed
+        elif keys[pygame.K_RIGHT] and SHIP_HITBOX.x + ship_speed + DEFAULT_WIDTH < width: # [RIGHT]
+            SHIP_HITBOX.x += ship_speed
+        
+        # [ENEMY MOVEMENT]
+        sprites['ENEMY1'].enemy_move(width)
+        sprites['ENEMY2'].enemy_move(width)
+        sprites['ENEMY3'].enemy_move(width)
+
+        change_window(LASERS=LASERS, SHIP=sprites['SHIP'], SHIP_HITBOX=SHIP_HITBOX, ENEMY1=sprites['ENEMY1'], ENEMY2=sprites['ENEMY2'], ENEMY3=sprites['ENEMY3'])
+    pygame.quit() # cant quit by pressing red X without this line or the if statement above
+
+# start point of the program
+if __name__ == "__main__":
+    # [OBJECTS]
+    SHIP = charachters.Charachter(DEFAULT_WIDTH, DEFAULT_HEIGHT, SHIP_X, SHIP_Y, image_handler.SHIP, WINDOW)
+    #BARRIER = charachters.multiple_charachters(barriers, DEFAULT_WIDTH, DEFAULT_HEIGHT, BARRIER_X, BARRIER_Y, image_handler.BARRIER, WINDOW)
+
+    ENEMY1 = charachters.enemy(enemy1, DEFAULT_WIDTH, DEFAULT_HEIGHT, ENEMY1_X, ENEMY1_Y, image_handler.ENEMY_1, WINDOW, enemy_speed, direction)
+    ENEMY1.generator(11, 65) 
+    ENEMY2 = charachters.enemy(enemy2, DEFAULT_WIDTH, DEFAULT_HEIGHT, ENEMY2_X, ENEMY2_Y, image_handler.ENEMY_2, WINDOW, enemy_speed, direction)
+    ENEMY2.generator(11, 65)
+    ENEMY3 = charachters.enemy(enemy3, DEFAULT_WIDTH, DEFAULT_HEIGHT, ENEMY3_X, ENEMY3_Y, image_handler.ENEMY_3, WINDOW, enemy_speed, direction)
+    ENEMY3.generator(11, 65)
+
+    main(SHIP=SHIP, ENEMY1=ENEMY1, ENEMY2=ENEMY2, ENEMY3=ENEMY3, MYSTERY='')
