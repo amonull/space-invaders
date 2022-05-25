1ca71525fdd4879a9d9450d6680ccb70739eb2bb
amonull <90932550+amonull@users.noreply.github.com>
2022-05-25 10:12:24 +0100

    bug fix
    fixed bugs that involeded main screen buttons and enemy movement starting too fast

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from scores import save_score
 from fonts import font_handler
 import charachters
 
+# gets the path of the parent directory
 parent_path = pathlib.Path(__file__).parent.absolute()
 
 pygame.init() # safely initilizing all pygame modules
@@ -63,7 +64,6 @@ def winning(score: str): # the charachters.menus is the screen while the nested 
         pygame.quit()
         sys.exit()
     charachters.menus(WINDOW, font_handler.SPACE_INVADERS_FONT, text_colour= WHITE, button1_colour=DARK_BLUE, button2_colour=DARK_BLUE).create_menu(quit_with_save, quit_without_save, main_text="Game Over", win_or_loss="You Won!", button_text1="Quit With Saving", button_text2="Quit Without Saving")
-
 def losing(score: str):
     """screen to come up when the player looses"""
     def quit_with_save():
@@ -75,6 +75,9 @@ def losing(score: str):
         sys.exit()
     charachters.menus(score, WINDOW, font_handler.SPACE_INVADERS_FONT, text_colour= WHITE, button1_colour=DARK_BLUE, button2_colour=DARK_BLUE).create_menu(quit_with_save, quit_without_save, main_text="Game Over", win_or_loss="You Lost!", button_text1="Quit With Saving", button_text2="Quit Without Saving")
 
+# used for a button so needs to be function
+def open_html(): webbrowser.open(f'{parent_path}/help.html')
+
 def change_window(**hit_box):
     """draw sprites to screen"""
     WINDOW.blit(image_handler.BG_IMG,(0,0)) #always keep on top so images added later on are not behind background image
@@ -84,10 +87,14 @@ def change_window(**hit_box):
     hit_box['LASERS'].handle_lasers(laser_speed, enemy_laser, laser_type, enemy_laser_list) #creates and handles lasers movement
     pygame.display.update()
 
-def main(**sprites):
+def main():
     """main function that runs the program"""
-    SHIP_HITBOX = sprites['SHIP'].hitbox() # ship.hitbox() is not called in __name__="__main__" since it is also needed in change_window to call draw_charachter
+    # [OBJECTS]
+    SHIP = charachters.Charachter(DEFAULT_WIDTH, DEFAULT_HEIGHT, SHIP_X, SHIP_Y, WINDOW)
+    SHIP_HITBOX = SHIP.hitbox() # ship.hitbox() is not called in __name__="__main__" since it is also needed in change_window to call draw_charachter
     LASERS = charachters.projectile(SHIP_HITBOX, WINDOW, score, 5, 15) # LASER is here and not in __name__="__main__" since it needs to use the hitbox of the ship
+    ENEMY = charachters.enemy(DEFAULT_WIDTH, DEFAULT_HEIGHT, WINDOW, direction, enemy_blocks, enemy_speed)
+    ENEMY.seperator()
 
     clock = pygame.time.Clock()
     run = True
@@ -120,7 +127,7 @@ def main(**sprites):
         try:
             # the ValueError occurs at max_enemy_x variable on enemy_move i would perfer to handle it there just with the affected variables however then i would have a to find another way to find if the user won or not instead of just checking if the lists are empty and if they return an error when they are
             # not the optimal way to handle an error like this however this way of handling it instead of handling it enemy_move() function makes saving user score easier
-            win_or_loss = sprites['ENEMY'].enemy_move(width, DEFAULT_WIDTH, current_time) # runs the function and puts the returned value into a variable
+            win_or_loss = ENEMY.enemy_move(width, DEFAULT_WIDTH, current_time) # runs the function and puts the returned value into a variable
 
             if max(sorted(win_or_loss)).y + DEFAULT_HEIGHT >= height: # check if player lost
                 losing(LASERS.score)
@@ -137,62 +144,10 @@ def main(**sprites):
             LASER_TYPE, ENEMY_LASER_OBJECT = ENEMY_LASER.shoot() # shoots the enemy laser when enemy laser list is empty
         
 
-        change_window(LASERS=LASERS, SHIP=sprites['SHIP'], SHIP_HITBOX=SHIP_HITBOX, ENEMY=sprites['ENEMY'], ENEMY_LASER=ENEMY_LASER, LASER_TYPE=LASER_TYPE, ENEMY_LASER_OBJECT=ENEMY_LASER_OBJECT)
+        change_window(LASERS=LASERS, SHIP=SHIP, SHIP_HITBOX=SHIP_HITBOX, ENEMY=ENEMY, ENEMY_LASER=ENEMY_LASER, LASER_TYPE=LASER_TYPE, ENEMY_LASER_OBJECT=ENEMY_LASER_OBJECT)
     #save_score.add_scores(str(LASERS.score)) # only runs when pygame is quit by pressing x button i dont know if i shoul add this as it would save user score before the game is officially finished (saves when user quits)
     pygame.quit() # quits pygame when out of loop
 
 # start point of the program
 if __name__ == "__main__":
-    # [OBJECTS]
-    SHIP = charachters.Charachter(DEFAULT_WIDTH, DEFAULT_HEIGHT, SHIP_X, SHIP_Y, WINDOW)
-
-    ENEMY = charachters.enemy(DEFAULT_WIDTH, DEFAULT_HEIGHT, WINDOW, direction, enemy_blocks, enemy_speed)
-    ENEMY.seperator()
-
-    menu_run = True
-    BUTTON_COLOR=DARK_BLUE
-    while menu_run: # start screen is made this way and not with class menus due to this window having to use main function that has parameters. the menus class cannot handle arguments on button functions as of right now
-        # creats new background image to overwrite anything underneath
-        WINDOW.blit(image_handler.BG_IMG,(0,0))
-
-        menu_text = font_handler.output(font_handler.SPACE_INVADERS_FONT, 60, "PLAY THE GAME", WHITE) # large menu text
-        # the self.x an self.y calculations are done to ensure text is in the middle of the screen
-        # the {menu_text.get_rect(center = self.window.get_rect().center).x} is used to find the center (X axis wise) part of the screen
-        WINDOW.blit(menu_text, (menu_text.get_rect(center = WINDOW.get_rect().center).x, 40)) # places it on screen
-
-        win_or_loss_text = font_handler.output(font_handler.SPACE_INVADERS_FONT, 45, "PLAY", WHITE)
-        WINDOW.blit(win_or_loss_text, (win_or_loss_text.get_rect(center = WINDOW.get_rect().center).x, 120))
-
-        basic_button1 = charachters.buttons(200, 85, 300, 375, WINDOW, font_handler.SPACE_INVADERS_FONT, button_colour=BUTTON_COLOR, text_colour=WHITE)
-        # button text has vauge naming so i can use it for any menu anywhere and still match the name to the button
-        basic_button1.draw(text="PLAY", outline=(0,0,0)) # outlines (0,0,0) is the colour black
-
-        basic_button2 = charachters.buttons(200, 85, 300, 475, WINDOW, font_handler.SPACE_INVADERS_FONT, button_colour=BUTTON_COLOR, text_colour=WHITE)
-        basic_button2.draw(text="HELP", outline=(0,0,0))
-
-        for event in pygame.event.get():
-
-            menu_run = False if event.type == pygame.QUIT else True
-
-            # [BUTTON ACTIONS]
-            if event.type == pygame.MOUSEBUTTONDOWN: # checks for mouse button click action
-                if basic_button1.isHover():
-                    # runs main function
-                    main(SHIP=SHIP, ENEMY=ENEMY, MYSTERY='')
-                if basic_button2.isHover():
-                    webbrowser.open(f'{parent_path}/help.html')
-            
-            if event.type == pygame.MOUSEMOTION: # checks for mouse button hover action
-                if basic_button1.isHover():
-                    BUTTON_COLOR = (25,16,43) # if hovering on top of the button changes the colour to a light purple
-                else:
-                    BUTTON_COLOR = DARK_BLUE # this format of changing the colours was choosen as it was the easiest (if not hover changes the colour back to dark purple)
-
-                if basic_button2.isHover():
-                    BUTTON_COLOR = (25,16,43)
-                else:
-                    BUTTON_COLOR = DARK_BLUE
-
-        pygame.display.update()
-    pygame.quit()
-    sys.exit() # sys.exit() needed to avoid {pygame.error: display Surface quit} error
+    charachters.menus(WINDOW, font_handler.SPACE_INVADERS_FONT, text_colour= WHITE, button1_colour=DARK_BLUE, button2_colour=DARK_BLUE).create_menu(main, open_html, main_text="Spac Invaders", win_or_loss="Start Game", button_text1="Start", button_text2="Help")
